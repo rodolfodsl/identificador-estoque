5421f1c9b7dca972e92d1a08a6641940a73f36e2
rodolfodsl <rodolf.cent@gmail.com>
2026-08-17 02:44:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ st.set_page_config(page_title="Identificador Visual e Consulta", layout="centere
 st.title("🧠 Identificador Visual & Consulta por Código")
 
 # =====================================================================
-# 1. COLOQUE SUA CHAVE DO GOOGLE AQUI (A MESMA QUE FUNCIONOU NA BARRA LATERAL)
+# CHAVE DO GOOGLE INCORPORADA COM SUCESSO (COMPLETA E FIXA)
 # =====================================================================
-CHAVE_GOOGLE_FIXA = "AQ.Ab8RN6L8veXzF6BWmIher3zMH5kdgCIjqXUT3eKAWu4wLH6fwg"
+CHAVE_GOOGLE_FIXA = "AQ.Ab8RN6L8veXzF6BWmlher3zMH5kdgCIjqXUT3eKAWu4wLH6fwg"
 
 # --- CREDENCIAIS FIXAS DO BLING ---
 CLIENT_ID = "416443567d77b7d8eb18a6f15e6e207f21d1d534".strip()
